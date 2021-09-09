5ac6a7729b9328869d7a8a8fd272bfdcc27f9c34
Sakshi <sakshishetty1231997@gmail.com>
2021-09-09 16:47:47 +0530

    Added: Welcome message to the program

---
+++ src/main/java/com/bridgelabz/CensusAnalyser.java
@@ -0,0 +1,12 @@
+package com.bridgelabz;
+/**
+ * Purpose - To implement Indian States Census Analyser
+ * @author - Sakshi Shetty
+ * @version - 16.0
+ * @since - 2021-09-09
+ */
+public class CensusAnalyser {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Program");
+    }
+}
