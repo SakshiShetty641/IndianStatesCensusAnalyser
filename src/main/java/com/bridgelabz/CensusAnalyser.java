2e3f3d67e9ba825daf0e53a043d4611000eef752
Sakshi <sakshishetty1231997@gmail.com>
2021-09-11 20:10:32 +0530

    Added: To load data from the CSV file

--- src/main/java/com/bridgelabz/CensusAnalyser.java
+++ src/main/java/com/bridgelabz/CensusAnalyser.java
@@ -1,4 +1,10 @@
 package com.bridgelabz;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+
 /**
  * Purpose - To implement Indian States Census Analyser
  * @author - Sakshi Shetty
@@ -6,7 +12,21 @@ package com.bridgelabz;
  * @since - 2021-09-09
  */
 public class CensusAnalyser {
-    public static void main(String[] args) {
-        System.out.println("Welcome to the Program");
+    public static void main(String[] args) throws IOException, CensusException {
+        String filePathRead = "C:\\Users\\sandesh shetty\\IdeaProjects\\IndianStatesCensusAnalyser\\IndianStateCensusData";
+        String fileName = "IndianStateCensusData";
+        String delimiter =",";
+        List<String> stringName = new ArrayList<>();
+        stringName.add("State");
+        stringName.add("Population");
+        stringName.add("AreaInSqKm");
+        stringName.add("DensityPerSqKm");
+
+        ReadOperation readObj = new ReadOperation();
+        int count = readObj.readDataCount(filePathRead, fileName);
+        System.out.println(count);
+
+        readObj.readDelimiter(filePathRead, delimiter);
+        readObj.readHeader(filePathRead, stringName);
     }
 }
